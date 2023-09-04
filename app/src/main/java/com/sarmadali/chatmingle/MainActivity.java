17a86839cfbaafbf95aa0690c7e0ffd718a11f1f
Sarmad Ali Jalbani <developer.sarmadali@gmail.com>
2023-09-04 11:47:36 +0500

    functionality: signin, toobarmenu, logout added!

--- app/src/main/java/com/sarmadali/chatmingle/MainActivity.java
+++ app/src/main/java/com/sarmadali/chatmingle/MainActivity.java
@@ -1,19 +1,71 @@
 package com.sarmadali.chatmingle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.sarmadali.chatmingle.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding mainBinding;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mainBinding.getRoot());
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //3 vertical dots in toolbar color change
+        toolbar.getOverflowIcon().setColorFilter(Color.WHITE , PorterDuff.Mode.SRC_ATOP);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+    }
+
+    //to inflate the menu && toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.toolbarmenu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+    //action on menu item clicked
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.newgroup) {
+            Toast.makeText(this, "This is New Group", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.newbroadcast) {
+            Toast.makeText(this, "This is New Broadcast", Toast.LENGTH_SHORT).show();
+        }
+        //for Sign out
+        else if (itemId == R.id.logout) {
+
+            firebaseAuth.signOut();
+            Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+            startActivity(intent);
+        }
+        return true;
     }
 }
--- app/src/main/java/com/sarmadali/chatmingle/SignInActivity.java
+++ app/src/main/java/com/sarmadali/chatmingle/SignInActivity.java
@@ -1,16 +1,26 @@
 package com.sarmadali.chatmingle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.sarmadali.chatmingle.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
 
     ActivitySignInBinding signInBinding;
+    ProgressDialog progressDialog;
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +36,49 @@ public class SignInActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //for progress dialogue after signup if success or not
+        progressDialog = new ProgressDialog(SignInActivity.this);
+        progressDialog.setTitle("Login");
+        progressDialog.setMessage("Login to your account!");
+
+        //for sign in functionality
+        signInBinding.buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //show progress dialogue
+                progressDialog.show();
+
+                firebaseAuth.signInWithEmailAndPassword(
+                        signInBinding.textEmailsignin.getText().toString(),
+                        signInBinding.textPasswordsignin.getText().toString()
+                        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //close progress dialogue
+                        progressDialog.dismiss();
+
+                        if (task.isSuccessful()){
+                            Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(SignInActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
+        // if user is already login then start with mainActivity
+        if (firebaseAuth.getCurrentUser()!=null){
+            Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+
     }
 }
